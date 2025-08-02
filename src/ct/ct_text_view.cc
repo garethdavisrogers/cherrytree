8290b59b21d631aafc26aeec42226c83a6f594eb
Gareth <garethdavisrogers@gmail.com>
2025-08-02 21:58:19 +0000

    Added condition for first line

--- src/ct/ct_text_view.cc
+++ src/ct/ct_text_view.cc
@@ -673,8 +673,9 @@ void CtTextView::for_event_after_key_press(GdkEvent* event, const Glib::ustring&
     }
     else if(GDK_KEY_BackSpace == event->key.keyval || GDK_KEY_Delete == event->key.keyval){
         Gtk::TextIter iter = text_buffer->get_insert()->get_iter();
+        int line_number = iter.get_line();
         int line_offset = iter.get_line_offset();
-        if(line_offset == 0  && iter.forward_line()){
+        if(line_number != 0 && line_offset == 0  && iter.forward_line()){
             std::cout << "Line offset: "<< line_offset << " and is forward line" << std::endl;
         }
     }
